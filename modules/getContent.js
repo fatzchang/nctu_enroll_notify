c9276540c891d0e00b1d82a2793420caf8f74fce
fatz <github@fatz.tw>
2020-05-09 00:32:57 +0800

    fetch newest data
    fetch newest 
    __VIEWSTATE, 
    __EVENTVALIDATION, 
    __VIEWSTATEGENERATOR 
    
    and use them to do second fetch

--- modules/getContent.js
+++ modules/getContent.js
@@ -1,22 +1,29 @@
 var request = require('request');
-var nctu = require('../configs/nctu');
+var cheerio = require('cheerio');
 
 // get page html
 function getContent(callback) {
-  var { views, validation } = nctu;
-
-  var body = {
+  var formBody = {
     ddlExamList: '60781cbf-44ce-4bfa-9490-a7eabfca7abb',
     ddlExamType: 'a782c851-8dbc-41ed-97eb-b7c16e0de9cc',
     __EVENTTARGET: 'ddlExamList',
     __EVENTARGUMENT: '',
     __LASTFOCUS: '',
-    __VIEWSTATE: views,
-    __VIEWSTATEGENERATOR: '2B30844A',
-    __EVENTVALIDATION: validation
+    __VIEWSTATE: '',
+    __VIEWSTATEGENERATOR: '',
+    __EVENTVALIDATION: ''
   }
 
-  request.post('https://enroll.nctu.edu.tw/', { form: body }, callback)
+  request('https://enroll.nctu.edu.tw/', (err, res, body) => {
+    // 不帶參數取得原始頁面
+    var $ = cheerio.load(body);
+    formBody.__VIEWSTATE = $('#__VIEWSTATE').val();
+    formBody.__EVENTVALIDATION = $("#__EVENTVALIDATION").val();
+    formBody.__VIEWSTATEGENERATOR = $("#__VIEWSTATEGENERATOR").val();
+
+    // 帶入參數取得指定頁面
+    request.post('https://enroll.nctu.edu.tw/', { form: formBody }, callback)
+  });
 }
 
 module.exports = getContent;
